f5fe0e3725d18c5e8b5c53932bb368f50a367853
haruki <meteo.haru@gmail.com>
2023-12-10 17:46:51 +0000

    update

--- linovel/download_config.sh
+++ linovel/download_config.sh
@@ -12,5 +12,8 @@ pushd "src" || exit 1
 wget -O "download.py" "https://raw.githubusercontent.com/haruki-nikaidou/interesting-script/main/linovel/download.py"
 
 echo "==========================="
-echo "准备完成，请修改 download.py"
+echo "准备完成，请修改 src/download.py"
 echo "==========================="
+echo "修改对应参数后，在src目录运行以下命令"
+echo "source ../venv/bin/activate"
+echo "python3 download.py"
